a417b86eefff6eb341c763d304098e5d5c762593
Swagatam Dalei <swagatam9957@gmail.com>
2026-06-10 11:19:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ msg.innerHTML="Message Sent Successfully ✅";
 form.reset();
 
 });
+window.onload = function () {
+    document.querySelector(".html").style.width = "90%";
+    document.querySelector(".css").style.width = "85%";
+    document.querySelector(".js").style.width = "75%";
+    document.querySelector(".node").style.width = "70%";
+    document.querySelector(".mongo").style.width = "65%";
+};
